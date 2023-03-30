e1af25d0bc4b95a0c49d1c47601816197d397031
lenevo <lenevo@DESKTOP-4EI3EN9>
2023-03-30 16:14:18 +0530

    FIRST COMMIT

---
+++ Hi/src/introduction/hello.java
@@ -0,0 +1,13 @@
+package introduction;
+
+public class hello
+{
+	public static void main(String args[]){
+    System.out.println("welcome");
+	System.out.println("nice to meet you");
+	System.out.println("hey");
+	}
+	
+	
+
+}
